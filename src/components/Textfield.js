4fd32cff52c90d3f849238524d6e07afd2e5b70c
abhilashc1920 <abhilash.c1920@gmail.com>
2022-05-10 18:52:37 +0530

    fisrst

--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -4,10 +4,20 @@ function Textfields(props){
     const styles = {
         container: 'color: red;',
     }
+    const yellowButton = css`
+    color: #424246;
+    background-color: #f9d648;
+  `;
+    const [colors, setcolors] = useState();
+
+    const setStyle =(color)=>{
+        color
+    }
 
 return(
+
     <div>
-        <input placeholder={props.placeholder} color={styles.container}/>
+        <input placeholder={props.placeholder} color={colors}/>
     </div>
 )
 }
