59180b6f7f0f202864301123b9a2be19f9d249b1
abhilashc1920 <abhilash.c1920@gmail.com>
2022-05-28 16:27:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,24 +16,24 @@ class App extends React.Component {
     );
   }
 
-  renderContent(){
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <div><Spinner message="Please acceot location request"/></div>
+    return <div><Spinner message="Please acceot location request" /></div>
 
   }
 
   render() {
 
-  return(
-    <div>
-    {this.renderContent()}
-    </div>
-  )
+    return (
+      <div className='border red'>
+        {this.renderContent()}
+      </div>
+    )
 
   }
 }
